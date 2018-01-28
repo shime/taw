3e767f5771ff515845e11a0f20271dded24f0d4f
Hrvoje Šimić <shime@twobucks.co>
2018-01-28 12:52:44 +0100

    feat: add support for singular values

--- lib/taw.rb
+++ lib/taw.rb
@@ -5,51 +5,59 @@ module Taw
   HOUR_IN_SECONDS = MINUTE_IN_SECONDS * 60
   DAY_IN_SECONDS = HOUR_IN_SECONDS * 24
 
-  extend self
-
-  def time_ago_in_words(time)
-    self.distance = Time.now - time
-    calculate_distance
-    output_distance
+  def self.time_ago_in_words(time)
+    Calculator.new.time_ago_in_words(time)
   end
 
-  private
+  class Calculator
+    def time_ago_in_words(time)
+      self.distance = Time.now - time
+      calculate_distance
+      output_distance
+    end
 
-  attr_accessor :hours, :minutes, :seconds, :distance
+    private
 
-  def calculate_distance
-    while distance > 0
-      if distance < MINUTE_IN_SECONDS
-        build_seconds
-      end
+    attr_accessor :hours, :minutes, :seconds, :distance
 
-      if distance < HOUR_IN_SECONDS && distance > 0
-        build_minutes
-      end
+    def calculate_distance
+      while distance > 0
+        if distance < MINUTE_IN_SECONDS && distance > 0
+          build_seconds
+        end
 
-      if distance < DAY_IN_SECONDS && distance > 0
-        build_hours
+        if distance < HOUR_IN_SECONDS && distance > 0
+          build_minutes
+        end
+
+        if distance < DAY_IN_SECONDS && distance > 0
+          build_hours
+        end
       end
     end
-  end
 
-  def build_seconds
-    self.seconds, self.distance = distance.to_i, 0
-  end
+    def build_seconds
+      self.seconds, self.distance = distance.to_i, 0
+    end
 
-  def build_minutes
-    self.minutes, self.distance = distance.divmod(MINUTE_IN_SECONDS)
-  end
+    def build_minutes
+      self.minutes, self.distance = distance.divmod(MINUTE_IN_SECONDS)
+    end
 
-  def build_hours
-    self.hours, self.distance = distance.divmod(HOUR_IN_SECONDS)
-  end
+    def build_hours
+      self.hours, self.distance = distance.divmod(HOUR_IN_SECONDS)
+    end
 
-  def output_distance
-    [
-      ("#{hours} hours" if hours && hours > 0),
-      ("#{minutes} minutes" if minutes && minutes > 0),
-      ("#{seconds} seconds" if seconds && seconds > 0)
-    ].compact.join(" and ")
+    def output_distance
+      [
+        ("#{hours} #{pluralize(hours, "hour", "hours")}" if hours && hours > 0),
+        ("#{minutes} #{pluralize(minutes, "minute", "minutes")}" if minutes && minutes > 0),
+        ("#{seconds} #{pluralize(seconds, "second", "seconds")}" if seconds && seconds > 0)
+      ].compact.join(" and ")
+    end
+
+    def pluralize(count, singular, plural)
+      count > 1 ? plural : singular
+    end
   end
 end
--- test/taw_test.rb
+++ test/taw_test.rb
@@ -2,14 +2,18 @@ require_relative "test_helper"
 
 class TawTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Taw::VERSION
+    refute_nil Taw::VERSION
   end
 
   def test_it_returns_hours
-    assert_equal "2 hours", ::Taw.time_ago_in_words(Time.now - 60 * 60 * 2)
+    assert_equal "2 hours", Taw.time_ago_in_words(Time.now - 60 * 60 * 2)
   end
 
   def test_it_returns_second_when_applicable
-    assert_equal "2 minutes and 3 seconds", ::Taw.time_ago_in_words(Time.now - 123)
+    assert_equal "2 minutes and 3 seconds", Taw.time_ago_in_words(Time.now - 123)
+  end
+
+  def test_it_returns_singulars
+    assert_equal "1 minute and 1 second", Taw.time_ago_in_words(Time.now - 61)
   end
 end

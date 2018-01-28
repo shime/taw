46ecd75c3d5090232c46307ceaa0dcdaa9c9f2e0
Hrvoje Šimić <shime@twobucks.co>
2018-01-28 12:36:48 +0100

    feat: start calculating time distance in hours, minutes and seconds

--- lib/taw.rb
+++ lib/taw.rb
@@ -1,5 +1,55 @@
 require "taw/version"
 
 module Taw
-  # Your code goes here...
+  MINUTE_IN_SECONDS = 60
+  HOUR_IN_SECONDS = MINUTE_IN_SECONDS * 60
+  DAY_IN_SECONDS = HOUR_IN_SECONDS * 24
+
+  extend self
+
+  def time_ago_in_words(time)
+    self.distance = Time.now - time
+    calculate_distance
+    output_distance
+  end
+
+  private
+
+  attr_accessor :hours, :minutes, :seconds, :distance
+
+  def calculate_distance
+    while distance > 0
+      if distance < MINUTE_IN_SECONDS
+        build_seconds
+      end
+
+      if distance < HOUR_IN_SECONDS && distance > 0
+        build_minutes
+      end
+
+      if distance < DAY_IN_SECONDS && distance > 0
+        build_hours
+      end
+    end
+  end
+
+  def build_seconds
+    self.seconds, self.distance = distance.to_i, 0
+  end
+
+  def build_minutes
+    self.minutes, self.distance = distance.divmod(MINUTE_IN_SECONDS)
+  end
+
+  def build_hours
+    self.hours, self.distance = distance.divmod(HOUR_IN_SECONDS)
+  end
+
+  def output_distance
+    [
+      ("#{hours} hours" if hours && hours > 0),
+      ("#{minutes} minutes" if minutes && minutes > 0),
+      ("#{seconds} seconds" if seconds && seconds > 0)
+    ].compact.join(" and ")
+  end
 end
--- test/taw_test.rb
+++ test/taw_test.rb
@@ -1,11 +1,15 @@
-require "test_helper"
+require_relative "test_helper"
 
 class TawTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Taw::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_returns_hours
+    assert_equal "2 hours", ::Taw.time_ago_in_words(Time.now - 60 * 60 * 2)
+  end
+
+  def test_it_returns_second_when_applicable
+    assert_equal "2 minutes and 3 seconds", ::Taw.time_ago_in_words(Time.now - 123)
   end
 end
